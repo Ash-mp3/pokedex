d012a5c7a81dd72665ad8de9d87aa0e5e7e7a8f8
Asher Contreras <asher.contreras1480@stu.mtec.edu>
2023-11-02 01:01:31 -0600

    file struc and base css layout

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.src.css'; 
+// link to main css file
+
+function App() {
+  return (
+    <div className='dex'>
+      <div className='dexScreen'>
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
