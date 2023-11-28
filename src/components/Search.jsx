5662a64b4126595e29c4c161b2a1159373b1df47
Asher Contreras <asher.contreras1480@stu.mtec.edu>
2023-11-28 10:30:39 -0700

    type selector implemented, still not functinal.

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,18 @@
+import { useContext, useEffect, useState } from "react";
 import { TextField } from "@mui/material";
-import { useContext } from "react";
 import InputAdornment from "@mui/material/InputAdornment";
 import pokeMag from "../images/pokemon_mag.png";
 import { PokeListContext } from "./PokeContext";
+import Select from "@mui/material/Select";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
 
 const Search = () => {
+  const [typeList, setTypeList] = useState([]);
+  const [currType, setCurrType] = useState("");
+
   const { state, dispatch } = useContext(PokeListContext);
   const handleSearch = (e) => {
     if (e.key === "Enter") {
@@ -25,11 +33,27 @@ const Search = () => {
     }
   };
 
+  useEffect(() => {
+    const getTypeOptions = async () => {
+      const res = await fetch("https://pokeapi.co/api/v2/generation/1/");
+      const data = await res.json();
+      const types = [];
+      data.types.forEach((type) => {
+        types.push(type.name);
+        setTypeList([...types]);
+      });
+    };
+    getTypeOptions();
+  }, []);
+
   return (
     <>
       <TextField
         label="Search Pokemon"
-        id="text"
+        placeholder="Search"
+        variant="outlined"
+        type="search"
+        onKeyDown={handleSearch}
         InputProps={{
           startAdornment: (
             <InputAdornment position="start">
@@ -41,11 +65,26 @@ const Search = () => {
             </InputAdornment>
           ),
         }}
-        placeholder="Search"
-        variant="outlined"
-        type="search"
-        onKeyDown={handleSearch}
       />
+      <Box sx={{ minWidth: 80 }}>
+        <FormControl fullWidth>
+          <InputLabel id="typeInput">type</InputLabel>
+          <Select
+            value={currType}
+            label="type"
+            onChange={(event) => {
+              setCurrType(event.target.value);
+            }}
+          >
+            <MenuItem value="">
+              <em>none</em>
+            </MenuItem>
+            {typeList.map((type) => {
+              return <MenuItem value={type} key={type}>{`${type}`}</MenuItem>;
+            })}
+          </Select>
+        </FormControl>
+      </Box>
     </>
   );
 };
